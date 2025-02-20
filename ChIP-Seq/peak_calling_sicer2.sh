8681e675e9436c5420cdfde5db68a43e7cdccb24
ocamach1 <ocamach1@jh.edu>
2025-02-20 10:48:25 -0500

    Update peak_calling_sicer2.sh

--- ChIP-Seq/peak_calling_sicer2.sh
+++ ChIP-Seq/peak_calling_sicer2.sh
@@ -1,9 +1,9 @@
 module load samtools
 
-##sicer2 requires bed files for both ChIP and Input samples
-#Sort bam files by name
+## sicer2 requires bed files for both ChIP and Input samples
+# Sort bam files by name
 samtools sort -n -o ${samp_name}_filtered_mouse_reads_final_nsorted.bam $outdir ${samp_name}_filtered_mouse_reads_final.bam
-#Convert bam to bed
+# Convert bam to bed
 bedtools bamtobed -i ${samp_name}_filtered_mouse_reads_final_nsorted.bam  > ${samp_name}_filtered_mouse_reads_final_nsorted.bed
 
 # Run sicer2 specifying ChIP and Input samples
