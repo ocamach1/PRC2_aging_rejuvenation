cf69a42ecf9c9cdbc4fe6187fb8852a3543895d8
ocamach1 <ocamach1@jh.edu>
2025-06-16 21:53:15 -0400

    Update peak_calling_sicer2.sh

--- ChIP-Seq/peak_calling_sicer2.sh
+++ ChIP-Seq/peak_calling_sicer2.sh
@@ -1,12 +1,15 @@
-module load samtools
+# Required packages:
+# - samtools
 
-## sicer2 requires bed files for both ChIP and Input samples
-# Sort bam files by name
-samtools sort -n -o ${samp_name}_filtered_mouse_reads_final_nsorted.bam $outdir ${samp_name}_filtered_mouse_reads_final.bam
-# Convert bam to bed
+# Sicer2 requires bed files for both IP and Input samples. bed files are obtained from the previously processed bam files containing reads uniquely aligned to the mouse genome.
+# Sorting bam files by read name
+samtools sort -n -o ${samp_name}_filtered_mouse_reads_final_nsorted.bam ${samp_name}_filtered_mouse_reads_final.bam
+# Converting bam to bed format
 bedtools bamtobed -i ${samp_name}_filtered_mouse_reads_final_nsorted.bam  > ${samp_name}_filtered_mouse_reads_final_nsorted.bed
 
-# Run sicer2 specifying ChIP and Input samples
+# Running Sicer2
+# -t expects the bed file containing the data from the IP sample
+# -c expects the bed file containing the data from the Input sample
 sicer -t ${samp_name_ChIP}_filtered_mouse_reads_final_nsorted.bed \
         -c ${samp_name_Input}_filtered_mouse_reads_final_nsorted.bed \
         --species mm10 \
