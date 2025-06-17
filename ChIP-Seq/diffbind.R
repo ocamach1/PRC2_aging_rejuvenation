e57cdb8e37c0d197645f3994b59bb4aa4ba9fefd
ocamach1 <ocamach1@jh.edu>
2025-06-16 22:31:34 -0400

    Update differential_binding_diffbind.R

--- ChIP-Seq/diffbind.R
+++ ChIP-Seq/diffbind.R
@@ -1,8 +1,13 @@
 library("DiffBind")
 library(csaw)
 
-# Read sample sheet with metadata of our samples 
-# (See diffbind documentation to prepare metadata sheet. We include paths for ChIP, input and spikein (reads aligned to Drosophila) bam files, and bed files with the coordinates of the peaks defined by sicer2)
+# Reading sample sheet with metadata of our samples. See DiffBind documentation to prepare metadata sheet. 
+# Briefly, the metadata sheet contains the paths for:
+# - IP bam file (containing reads uniquely aligned to the mouse genome, derived from the IP sample - DNA bound to the target of interest).
+# - Input bam file (containing reads uniquely aligned to the mouse genome, derived from the Input sample - Total chromatin DNA with no antibody pull-down).
+# - Spike-in bam file (containing reads uniquely aligning to Drosophila genome, derived from the IP sample). 
+# - BED file containing the coordinates of the peaks defined by SICER2.
+
 samples <- read.csv("peaksets_H3K27me3.csv")
 
 # Creating a dba object
@@ -11,12 +16,12 @@ aging_H3K27me3 <- dba(sampleSheet=samples)
 # Creating binding matrix with scores based on read counts for every sample (affinity scores)
 aging_H3K27me3 <- dba.count(aging_H3K27me3, summit=2000)
 
-# Normalization with spikein (using bam files with reads that uniquely aligned to the Drosophila genome, specified in the metadata sheet)
+# Normalization using spike-in normalization
 aging_H3K27me3 <- dba.normalize(aging_H3K27me3, spikein=TRUE) 
 
-# Defining the comparison we are interested
+# Defining the contrast we are interested (Old vs Young)
 aging_H3K27me3 <- dba.contrast(aging_H3K27me3, minMembers=2,
-                          reorderMeta=list(Condition="Young")) # Here we define the control condition
+                          reorderMeta=list(Condition="Young")) # Here we define 'Young' as the control condition
 
 # Performing the differential analysis 
 aging_H3K27me3 <- dba.analyze(aging_H3K27me3, method=DBA_DESEQ2)   
